3cfa311fee8e0aefa7e642654b6f79a70432dd03
Sam Yerkes <syerkes@vcu.edu>
2016-04-25 16:49:01 -0400

    added procedures to show top pages for websites and top websites according to visits

--- procedures/top_websites.sql
+++ procedures/top_websites.sql
@@ -1,6 +1,28 @@
-select websites.website_address, count(visits.visit_ip) as totalHits from websites, pages, visits
-  where websites.website_id = pages.website_id
-    and pages.page_id = visits.page_id
-    and visits.visit_timestamp > sysdate - 20
-  group by websites.website_address
-  order by totalHits desc;
+Create or replace PROCEDURE top_websites(dayssince in int) IS  
+  /* create a cursor to select the websites and their visit counts */
+  Cursor c1 is select websites.website_address, count(visits.visit_ip) as totalHits from websites, pages, visits
+      where websites.website_id = pages.website_id
+        and pages.page_id = visits.page_id
+        and visits.visit_timestamp > sysdate - dayssince
+      group by websites.website_address
+      order by totalHits desc;
+
+  /* declare variables */
+  websiteName varchar(200);
+  visitCount int;
+BEGIN
+  Open c1; -- open cursor
+  Loop -- loop through cursor results
+    fetch c1 into websiteName, visitCount;  -- fetch each result in to declared variables
+    exit when c1%notfound; -- exit the loop when no more results are found
+    dbms_output.put_line(websiteName || ': ' || visitCount || ' visits in the last ' || dayssince || ' days');  -- display operator output of top pages and their visit counts
+  End loop;  -- end the loop
+  Close c1;  -- close the cursor
+EXCEPTION
+   when no_data_found then
+     dbms_output.put_line('No visits were found for this system');
+END;
+
+Begin
+  top_websites(21); 
+End;
--- procedures/website_top_pages.sql
+++ procedures/website_top_pages.sql
@@ -1,16 +1,29 @@
-select pages.page_id, count(pages.page_id) as total_visits from websites, pages, visits
+Create or replace PROCEDURE website_top_pages(websiteid in int, dayssince in int) IS  
+  /* create a cursor to select the pages and their visit counts from the input parameter website */
+  Cursor c1 is select pages.page_name, count(pages.page_id) as total_visits from websites, pages, visits
   where websites.website_id = pages.website_id
     and pages.page_id = visits.page_id
-    and visits.visit_timestamp > sysdate - 20
-  group by visits.page_id
+    and visits.visit_timestamp > sysdate - dayssince
+    and websites.website_id = websiteid
+  group by pages.page_id, pages.page_name
   order by total_visits desc, pages.page_id;
 
+  /* declare variables */
+  pageName varchar(200);
+  visitCount int;
+BEGIN
+  Open c1; -- open cursor
+  Loop -- loop through cursor results
+    fetch c1 into pageName, visitCount;  -- fetch each result in to declared variables
+    exit when c1%notfound; -- exit the loop when no more results are found
+    dbms_output.put_line(pageName || ': ' || visitCount || ' visits in the last ' || dayssince || ' days');  -- display operator output of top pages and their visit counts
+  End loop;  -- end the loop
+  Close c1;  -- close the cursor
+EXCEPTION
+   when no_data_found then
+     dbms_output.put_line('No visits were found for that website');
+END;
 
-
-
-
-  select websites.website_id, websites.website_address, pages.page_id, visits.visit_ip from websites, pages, visits
-  where websites.website_id = pages.website_id
-    and pages.page_id = visits.page_id
-    and visits.visit_timestamp > sysdate - 20
-  order by websites.website_id, pages.page_id;
+Begin
+  website_top_pages(2, 14); 
+End;
